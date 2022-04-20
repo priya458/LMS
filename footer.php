a9f13ab15bc84960a7e6787eafd2521523795618
priyanka <priyankagadhave0303@gmail.com>
2022-04-20 16:59:20 +0530

    initial-commit

---
+++ footer.php
@@ -0,0 +1,8 @@
+<center>
+		<footer>
+		
+		<p>GCOEARA E-Learning Copyright 2022</p>
+		
+		</footer>
+</center>
+
---
+++ title_index.php
@@ -0,0 +1,40 @@
+				
+								<div class="row-fluid">
+
+						<div class="span12">
+						
+						</div>	
+													
+							</div>
+							<div class="row-fluid">
+
+						<div class="span4">
+						<img class="index_logo" src="admin/images/Logo.jpg">
+						</div>	
+						<div class="span8">
+						
+								<div class="title">
+							<p class="chmsc">Government College Of Engineering and Reasearch Avasari Khurd</p>
+							<h3>
+
+							<p>E - Learning</p>
+						
+							</h3>		
+						</div>
+			
+						</div>							
+							</div>
+				
+				<div class="row-fluid">
+
+						<div class="span12">
+						<br>
+								<div class="motto">
+												<p>GCOEARA</p>
+												
+												<address>Taluka:Ambegaon, District:Pune,
+												412405</address>
+			                                   
+								</div>		
+						</div>		
+				</div>
